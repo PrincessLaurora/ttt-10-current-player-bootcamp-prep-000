bdbafd906aabfc3dffe7e7586ecd862a970ca9aa
Laura Felce <crepu83@yahoo.it>
2018-10-04 08:41:52 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   counter  = 0
   board.each do |turn|
-  if "X" = 0  && "O" = "X" + 1
-    return turn
+  if turn == "X" || turn == "O"
        counter += 1
   end
 end
+return counter
 end
